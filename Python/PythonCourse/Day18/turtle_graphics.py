dc1c62cfaa7033ee0307ff11d91bb69580c82956
Martin Devenney <themadrebellion@gmail.com>
2021-03-23 18:55:26 +0000

    Learning more about turtle and graphics

---
+++ Python/PythonCourse/Day18/turtle_graphics.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+turt = Turtle()
+turt.speed(1)
+
+# Shapes Challenge
+# shape_sides = [3, 4, 5, 6, 7, 8, 9, 10]
+# shape_color = ["red", "orange", "yellow",
+#                "green", "blue", "indigo", "black", "salmon", "purple"]
+# index = 0
+
+# for side in shape_sides:
+#     turt.color(shape_color[index])
+#     for _ in range(shape_sides[index]):
+#         turt.forward(100)
+#         turt.right(360/shape_sides[index])
+
+#     index += 1
+
+# Random Walk
+
+
+win = Screen()
+win.exitonclick()
