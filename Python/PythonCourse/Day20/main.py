f9c01c645af649817908701df318d628cab26be1
Martin Devenney <themadrebellion@gmail.com>
2021-04-26 18:05:28 +0100

    tightened up some numbers. minor changes

--- Python/PythonCourse/Day20/main.py
+++ Python/PythonCourse/Day20/main.py
@@ -23,26 +23,28 @@ my_screen.onkey(snake.right, "Right")
 is_game_on = True
 while is_game_on:
     my_screen.update()
-    time.sleep(0.1)
+    time.sleep(0.075)
     snake.move()
 
+    # Food collision checks
     if snake.snake_obj[0].distance(food) < 20:
         food.refresh()
         snake.extend()
         score.update_score()
 
-    if snake.snake_obj[0].xcor() > 280 or snake.snake_obj[0].xcor() < -280 or snake.snake_obj[0].ycor() > 280 or snake.snake_obj[0].xcor() < -280:
+    # Wall collision checks
+    if snake.snake_obj[0].xcor() > 280 or snake.snake_obj[0].xcor() < -300 or snake.snake_obj[0].ycor() > 300 or snake.snake_obj[0].ycor() < -280:
         is_game_on = False
         score.game_over()
-        time.sleep(3)
+        # time.sleep(3)
 
-    for seg in snake.snake_obj:
-        if seg == snake.snake_obj[0]:
-            pass
-        elif snake.snake_obj[0].distance(seg) < 10:
+    # Slicing lists so that the head isn't considered
+    for seg in snake.snake_obj[1:]:
+        # if seg == snake.snake_obj[0]:
+        #     pass
+        if snake.snake_obj[0].distance(seg) < 10:
             is_game_on = False
             score.game_over()
-            time.sleep(3)
+            # time.sleep(3)
 
-#
-# my_screen.exitonclick()
+my_screen.exitonclick()
